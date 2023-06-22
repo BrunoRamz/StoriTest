0c0d329ede0abe7bcd78a131d79e5149e9f0c3ab
Bruno Ramz <bruce.iwan02@gmail.com>
2023-06-22 09:36:21 -0600

    [UPD]: Database methods.

--- db.py
+++ db.py
@@ -12,30 +12,31 @@ load_dotenv(find_dotenv())
 
 
 class DB:
-    def __init__(self):
-        self.database = os.getenv('DATABASE')
-    
     def start_database(self):
-        self.connection = sqlite3.connect(self.database)
+        self.connection = sqlite3.connect(os.getenv('DATABASE'))
         self.cursor = self.connection.cursor()
-    
+        print("Connection Established...")
+
     def create_table(self):
         self.cursor.execute(
             """
-            CREATE TABLE IF NOT EXISTS transaction
-            ([transaction_id] INTEGER PRIMARY KEY,
+            CREATE TABLE IF NOT EXISTS transactions
+            ([transactions_id] INTEGER PRIMARY KEY,
              [date] TEXT,
-             [debit_amount] FLOAT,
-             [redit_amount] FLOAT,
-             [total_balance] FLOAT
+             [transaction] FLOAT
             )
             """
         )
-    
-    def insert_data(self):
-        self.cursor.executemany(
+
+    def store_data(self):
+        pass
+
+    def show_table(self):
+        pass
+
+    def drop_table(self):
+        self.cursor.execute(
             """
-            INSERT INTO transaction(?,?,?,?,?)
+            DROP TABLE IF EXISTS transactions
             """
         )
-        
